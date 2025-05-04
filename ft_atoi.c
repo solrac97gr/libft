8420266e028ac920d25a53f21bb951f10f39bea5
solrac97gr <carlosgarcia_97gr@hotmail.com>
2025-05-05 00:58:22 +0200

    Add ft_atoi function implementation and test

---
+++ ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carloga2 <carloga2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 16:00:00 by carloga2          #+#    #+#             */
+/*   Updated: 2025/05/05 16:00:00 by carloga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Convert a string to an integer.
+ *
+ * This function converts the initial portion of the string pointed to by str
+ * to an integer representation. It skips leading whitespace characters,
+ * handles an optional +/- sign, and stops at the first non-digit character.
+ *
+ * @param str The string to be converted to an integer.
+ * @return int The converted integer value.
+ */
+int     ft_atoi(const char *str)
+{
+        int     result;
+        int     sign;
+        int     i;
+
+        result = 0;
+        sign = 1;
+        i = 0;
+        while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+                || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+                i++;
+        if (str[i] == '+' || str[i] == '-')
+        {
+                if (str[i] == '-')
+                        sign = -1;
+                i++;
+        }
+        while (str[i] >= '0' && str[i] <= '9')
+        {
+                result = result * 10 + (str[i] - '0');
+                i++;
+        }
+        return (result * sign);
+}
